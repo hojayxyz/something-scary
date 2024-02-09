e584bc3aa6a42d4863a4b72ffe6a2c460417628c
Hojay Lee <hojay@Hojays-MacBook-Air.local>
2024-02-09 21:26:45 +0700

    Loading text animation and upgrade

--- src/pages/Scary.jsx
+++ src/pages/Scary.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
+import Typewriter from "../utils/Typewriter";
 
 function Scary() {
   const [searchParams] = useSearchParams();
-  const randomNumber = searchParams.get("rn");
-  const [isScarying, setIsScarying] = useState(false);
+  const randomNumber = searchParams.get("hacking");
+  const [isScarying, setIsScarying] = useState(0);
   const [countdown, setCountdown] = useState(0);
   const [scared, setScared] = useState(0);
   const [progress, setProgress] = useState(0);
@@ -19,26 +20,27 @@ function Scary() {
   }
 
   function scaryCounter() {
-    setIsScarying(true);
+    setIsScarying(1);
     setScared(0);
     var i = setInterval(function () {
       // do your thing
       const newRandcomNumber =
         Math.round(Math.random() * 100000000) *
         Math.round(Math.random() * 100000000);
-      navi(`?rn=${newRandcomNumber}`);
+      navi(`?hacking=${newRandcomNumber}`);
       counter++;
       progress1();
       console.log("% hacked");
       setCountdown((c) => c + 1);
       if (counter === 100) {
-        setIsScarying(false);
+        setIsScarying(2);
         clearInterval(i);
         counter = 0;
         setCountdown(0);
         // setScared(10);
+        navi("/");
       }
-    }, 50);
+    }, 100);
   }
 
   return (
@@ -49,11 +51,22 @@ function Scary() {
         onLoaderFinished={() => setProgress(0)}
       />
       <h1 className="m-5">
-        {isScarying ? "Hacking...👻" : "Want something scary?"}
+        {isScarying === 1 ? (
+          <>
+            <span>Hacking</span>
+            <Typewriter text="..." delay={300} infinite />
+          </>
+        ) : isScarying === 0 ? (
+          "Want something scary?"
+        ) : (
+          "👻 Hacked 👻"
+        )}
       </h1>
-      {isScarying ? (
+      {isScarying === 1 ? (
         <div>
-          <h2 className="text-xl text-red-500 m-5">{randomNumber}</h2>
+          <h2 className="text-xl text-red-500 m-3">{randomNumber * 2}</h2>
+          <h2 className="text-xl text-red-500 m-3">{randomNumber * 3}</h2>
+          <h2 className="text-xl text-red-500 m-3">{randomNumber * 4}</h2>
           <p className="text-stone-500">Watch the url 🙄</p>
           <p className="m-2">{countdown}%</p>
           <button className="m-2" onClick={() => setScared((z) => z + 1)}>
@@ -63,14 +76,23 @@ function Scary() {
       ) : (
         <button onClick={scaryCounter}>Make it scary</button>
       )}
-      {scared === 0 && !isScarying ? (
+      {scared === 0 && isScarying === 0 ? (
         <div className="m-2 flex justify-center">Are you Ready?</div>
       ) : (
         <div className="m-2 flex flex-col justify-between">
           {scared > 0 ? <div className="m-2">Hehe</div> : null}
           {scared > 1 ? <div className="m-2">Scared?</div> : null}
-          {scared > 3 ? <div className="m-2">Hahahaha</div> : null}
-          {scared > 5 ? <div className="m-2">OWNED 😜</div> : null}
+          {scared > 2 ? <div className="m-2">Hahahaha</div> : null}
+          {scared > 3 ? <div className="m-2">OWNED 😜</div> : null}
+          {scared > 4 ? (
+            <div>
+              <p className="text-sm m-2 text-stone-500">
+                Relax
+                <br /> This is just a prank
+              </p>
+            </div>
+          ) : null}
+          {scared > 5 ? <div className="m-2 text-3xl">😇</div> : null}
         </div>
       )}
     </div>
